c726fe5029a17d9c84a87a1c6411b790eefb7ebc
Ihor Melnyk <92684157+Ihor-Melnyk@users.noreply.github.com>
2022-05-24 13:43:17 +0300

    reloadfiles

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+
+const containerGrig = document.querySelector('.gallery');
+const markup = createImages(galleryItems);
+containerGrig.insertAdjacentHTML('beforeend', markup);
+
+function createImages(galleryItems) {
+    return galleryItems.map(({ original, preview, description }) => {
+        return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+            `
+    }).join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData : 'alt',
+    captionDelay: '250',
+});
