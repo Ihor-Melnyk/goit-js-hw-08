b26e0f4a79f5d294256b4eeeed7763f08070f0bf
Ihor Melnyk <92684157+Ihor-Melnyk@users.noreply.github.com>
2022-05-26 20:01:14 +0300

    finishHim

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,33 @@
 import { throttle } from "lodash";
-import { mainModule } from "process";
 
+const KEY = 'feedback-form-state';
 const refs = {
     form: document.querySelector('.feedback-form'),
-    inputEmail: document.querySelector('input'),
-    inputMessage: document.querySelector('textarea'),
 };
-populateForm(); //повернення значень в поля
 
-const formData = {};
-
+refs.form.addEventListener('input', throttle(onInput, 500))
 refs.form.addEventListener('submit', onFormSubmit)
+ //повернення значень в поля
+checksLocalStorage();
+
+function onInput(e) {
+    const formData = localStorage.getItem(KEY)
+        ? JSON.parse(localStorage.getItem(KEY))
+        : {};
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(KEY, JSON.stringify(formData));  
+}
+
 function onFormSubmit(e) { 
     e.preventDefault();
     e.currentTarget.reset();
-    localStorage.removeItem("feedback-form-state");
-    console.log(formData);
+    localStorage.removeItem(KEY);
 }
 
-refs.form.addEventListener('input', throttle(onInput, 500))
-function onInput(e) {
-    formData[e.target.name] = e.target.value;
-    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
-    
-}
-
-function populateForm() {
-    let savedMessage = JSON.parse(localStorage.getItem("feedback-form-state"));
-    
-    if (savedMessage) { //перевірка на пусте значення
-        refs.inputMessage.value = savedMessage.message;
-        refs.inputEmail.value = savedMessage.email;
+function checksLocalStorage() {
+    const savedMessage = JSON.parse(localStorage.getItem(KEY));
+    if (savedMessage) {
+        refs.form.elements.email.value = savedMessage.email || '';
+        refs.form.elements.message.value = savedMessage.message || '';
     }
 }
